fed90043d831e38623dc7aa05b49eccbcd97b8ee
pndaza <mrpndaza@outlook.com>
2023-05-14 19:04:09 +0630

    add new 5 books

--- lib/data/constants.dart
+++ lib/data/constants.dart
@@ -2,5 +2,5 @@ class DatabaseInfo {
   DatabaseInfo._();
   static const String assetsPath = 'assets/database';
   static const String dbName = 'tika_nsy.db';
-  static const int dbVersion = 4;
+  static const int dbVersion = 5;
 }
